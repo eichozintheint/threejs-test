341bf44de72dd5724d01c780e9870d20868d897b
ei-cho-zin-theint <eichozintheint2559@gmail.com>
2026-07-30 16:08:24 +0900

    色修正アップ

--- lamborghini.js
+++ lamborghini.js
@@ -13,7 +13,7 @@ renderer.setPixelRatio(window.devicePixelRatio);
 
 // シーンを作成
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xF5F5F5);
+scene.background = new THREE.Color(0x000000);
 
 // カメラを作成
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
